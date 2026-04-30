8767b38980f60ea1b47d82bbf4339f8f8cec3c7f
Kirill Stetsenko <stetsenkoki@gmail.com>
2026-04-30 11:38:12 +0300

    replace appUrl in liqpay params

--- server/api/payments/liqpay.post.ts
+++ server/api/payments/liqpay.post.ts
@@ -6,8 +6,7 @@ export default defineEventHandler(async event => {
   const session = await requireUserSession(event)
   const body = await readBody(event)
 
-
-// декодирую для удобства потом буду через базу
+  // декодирую для удобства потом буду через базу
   const order_id = Buffer.from(
     JSON.stringify({
       userId: session.user.id,
@@ -28,7 +27,7 @@ export default defineEventHandler(async event => {
     description: `Upgrade to ${body.plan} Plan`,
     order_id,
     result_url: `${config.public.appUrl}/payment/success`,
-    server_url: `https://myron-unfrequentable-digna.ngrok-free.dev/api/payments/callback`
+    server_url: `${config.public.appUrl}/api/payments/callback`
   }
 
   const data = Buffer.from(JSON.stringify(params)).toString('base64')
