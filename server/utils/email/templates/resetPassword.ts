7075244cf5d9f5e4b63f15cc2edaf598a3790ded
Kirill Stetsenko <stetsenkoki@gmail.com>
2026-05-01 23:43:01 +0300

    fix reset email template link

--- server/utils/email/templates/resetPassword.ts
+++ server/utils/email/templates/resetPassword.ts
@@ -1,9 +1,11 @@
 import { Token } from '~~/prisma/generated/client'
 
+const config = useRuntimeConfig()
+
 export const resetPasswordTemplate = (token: Token) => {
   return `
           <h1 style="color:#111;">Reset password</h1>
           <p>Click the link below:</p>
-          <a href="http://localhost:3000/auth/reset-password?token=${token.token}">Reset password</a>
+          <a href="${config.public.appUrl}/auth/reset-password?token=${token.token}">Reset password</a>
         `
 }
